935053a933324cb11d3be3356db0e13457a11c9b
Júnior Moreira (Khaled) <khalegjr@gmail.com>
2021-11-22 00:27:17 -0300

    Adiciona o componente de listagem ao app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="container grid-lg">
-    <input v-model="quotes.test" />
-    <h1>{{ quotes }}</h1>
+  <div class="container grid-lg my-2 py-2">
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Todas as moedas</div>
+      </div>
+
+      <div class="card-body">
+        <list-quotes :quotes="quotes" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import { onMounted, reactive, toRefs } from "vue";
   import api from "@/services/api";
+  import ListQuotes from "./components/ListQuotes.vue";
+
   export default {
     name: "App",
+    components: { ListQuotes },
 
     setup() {
       const data = reactive({
-        quotes: {
-          test: "",
-        },
+        quotes: {},
       });
 
       onMounted(async () => {
