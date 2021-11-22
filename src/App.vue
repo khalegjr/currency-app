7d5612b79ac2d33e8dee51105a730edbbdc618b4
Júnior Moreira (Khaled) <khalegjr@gmail.com>
2021-11-21 23:09:11 -0300

    Mostra o resultado da chamada de todas as moedas

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="container grid-lg">
-    <h1>Hello World</h1>
+    <input v-model="quotes.test" />
+    <h1>{{ quotes }}</h1>
   </div>
 </template>
 
 <script>
+  import { onMounted, reactive, toRefs } from "vue";
+  import api from "@/services/api";
   export default {
     name: "App",
+
+    setup() {
+      const data = reactive({
+        quotes: {
+          test: "",
+        },
+      });
+
+      onMounted(async () => {
+        const response = await api.all();
+        data.quotes = response.data;
+      });
+
+      return { ...toRefs(data) };
+    },
   };
 </script>
